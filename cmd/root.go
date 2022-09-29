8c1731487e593981907fa400dfc66ec3612bb3da
Eric Haase <eric.haase@wpengine.com>
2022-09-28 22:37:46 -0500

    removed store command

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/erichaase/fantasy-collector/internal/espn"
 	"github.com/spf13/cobra"
 )
 
@@ -11,6 +13,15 @@ var rootCmd = &cobra.Command{
 	Use:   "fantasy-collector",
 	Short: "Collects and stores NBA stats from ESPN",
 	Long:  `Collects and stores NBA stats from ESPN`,
+	Run: func(cmd *cobra.Command, args []string) {
+		lines, err := espn.GetGameLines()
+		if err != nil {
+			// TODO: add a logger
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println(lines)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -23,4 +34,6 @@ func Execute() {
 }
 
 func init() {
+	// TODO: add --format flag: html, csv, etc.
+	// TODO: add --dest flag: s3, stdout, etc.
 }
